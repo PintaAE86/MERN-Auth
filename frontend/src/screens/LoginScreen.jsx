a842d8ddc02971497a9097b039e35882ca3f75f0
Parwinder Singh <131915695+PintaAE86@users.noreply.github.com>
2023-12-22 19:37:41 -0500

    updated header

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col} from 'react-bootstrap';
 import FormContainer from "../components/FormContainer";
+import { useDispatch, useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlices";
+import { toast } from 'react-toastify'
+
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const navigate = useNavigate();
+    const useDispatch = useDispatch();
+
+    const [login, {isLoading}] = useLoginMutation();
+
+    const { userInfo } = useSelector(() => state.auth);
+
+    useEffect(()=> {
+        if(userInfo){
+            navigate('/')
+        }
+    },[navigate, userInfo])
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        console.log('Submit');
+        try {
+            const res = await login({email, password}).unwrap();
+            dispatch(setCredentials({...res}))
+            navigate('/')
+        } catch (err) {
+            toast.error(err.data.message || err.error);
+        }
     }
 
   return (
